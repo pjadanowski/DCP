ce75f5877556774fa9e4ec04aa7b7e0b70141174
Pawel <pjadanowski@gmail.com>
2016-06-01 17:27:01 +0200

    design, okodowano

--- UDP_klient/Form1.Designer.cs
+++ UDP_klient/Form1.Designer.cs
@@ -29,36 +29,85 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Orange;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(69, 44);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(324, 458);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(295, 193);
+            this.button1.Size = new System.Drawing.Size(140, 48);
             this.button1.TabIndex = 0;
             this.button1.Text = "Click me ;-)";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(68, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(305, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(68, 412);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(675, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(68, 108);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(675, 277);
+            this.listBox1.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(546, 40);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(197, 20);
+            this.numericUpDown1.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PaleGreen;
-            this.ClientSize = new System.Drawing.Size(420, 295);
+            this.ClientSize = new System.Drawing.Size(805, 552);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
 
--- UDP_klient/Form1.cs
+++ UDP_klient/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
 
 namespace UDP_klient
 {
@@ -19,7 +20,23 @@ namespace UDP_klient
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("blablavblqas");
+            string host = textBox1.Text;
+            int port = System.Convert.ToInt16(numericUpDown1.Value);
+
+            try
+            {
+                UdpClient klient = new UdpClient(host, port);
+                Byte[] dane = Encoding.ASCII.GetBytes(textBox2.Text);
+                klient.Send(dane, dane.Length);
+                listBox1.Items.Add("Wysyłanie wiadomości do hosta " + host + ": " + port);
+                klient.Close();
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add("Błąd. Nie udało się wysłać wiadomości!");
+                MessageBox.Show(ex.ToString(), "Błąd");
+                throw;
+            }
         }
     }
 }
